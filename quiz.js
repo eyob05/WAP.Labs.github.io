241a24fd541ab569b4db03cb541f901dffaf3f0f
eyob05 <65434701+eyob05@users.noreply.github.com>
2020-10-11 10:01:32 -0500

    lab07,08,09

---
+++ quiz.js
@@ -0,0 +1,22 @@
+ "use strict";
+ function Foo(){
+    const counter1=function(){
+      let i=1;
+    };
+
+    let counter2=1;
+    }
+
+    const myFoo = new Foo();
+
+    Foo.extention=function(){
+
+        const add1=function(){
+            counter1 +=1;
+        }
+        const add2=function(){
+            counter2 +=1;
+        }
+    }
+    const bar=new Foo();    
+    
