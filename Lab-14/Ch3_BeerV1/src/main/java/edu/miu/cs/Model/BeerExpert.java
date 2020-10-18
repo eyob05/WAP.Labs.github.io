c436bede03420ea4b17e9ee6309f24d7d752711b
eyob05 <65434701+eyob05@users.noreply.github.com>
2020-10-18 01:46:51 -0500

    lab14&15

---
+++ Lab-14/Ch3_BeerV1/src/main/java/edu/miu/cs/Model/BeerExpert.java
@@ -0,0 +1,17 @@
+package edu.miu.cs.Model;
+import javax.servlet.annotation.WebServlet;
+import java.util.*;
+
+public class BeerExpert {
+	public List getBrands(String color) {
+		List brands = new ArrayList();
+		if (color.equals("amber")) {
+			brands.add("Jack Amber");
+			brands.add("Red Moose");
+		} else {
+			brands.add("Jail Pale Ale");
+			brands.add("Gout Stout");
+		}
+		return(brands);
+	}
+}
